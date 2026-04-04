d3d947914dff404f582505d5f25562c5fe661197
Sachin Pal <sachinpal9324@gmail.com>
2026-04-04 09:02:21 +0530

    Implement basic HTTP server for cloud simulation

---
+++ app.py
@@ -0,0 +1,82 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+
+class CloudService(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        parsed_url = urlparse(self.path)
+        params = parse_qs(parsed_url.query)
+
+        if parsed_url.path == "/":
+            self.show_form()
+
+        elif parsed_url.path == "/process":
+            self.process_request(params)
+
+        else:
+            # Handle unknown paths (IMPORTANT FIX)
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+    def show_form(self):
+        html = """
+        <html>
+        <body>
+            <h2>Cloud Computing Simulation</h2>
+            <form action="/process" method="get">
+                Enter your name:
+                <input type="text" name="name"><br><br>
+
+                Enter number of users:
+                <input type="number" name="users"><br><br>
+
+                <input type="submit" value="Send Request">
+            </form>
+        </body>
+        </html>
+        """
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html.encode())
+
+    def process_request(self, params):
+        name = params.get("name", ["Guest"])[0]
+
+        try:
+            users = int(params.get("users", [1])[0])
+        except ValueError:
+            users = 1
+
+        if users <= 10:
+            response = "Low Load"
+        elif users <= 50:
+            response = "Medium Load"
+        else:
+            response = "High Load – Scaling Needed!"
+
+        result = f"""
+        <html>
+        <body>
+            <h3>Hello {name}</h3>
+            <p>Users accessing service: {users}</p>
+            <p>System Status: <b>{response}</b></p>
+            <br><a href="/">Go Back</a>
+        </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(result.encode())
+
+
+# Start server
+if __name__ == "__main__":
+    server = HTTPServer(("127.0.0.1", 8000), CloudService)
+    print("Cloud service running on port 8000...")
+    server.serve_forever()
